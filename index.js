1337335b0034dfc535044abfca23bbab014fbeae
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-05-11 18:21:20 -0300

    feat: Criando função post checkout para recever os dados das compras, realizando assim as devidas validações com a lib joi assim como validações do token

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import chalk from "chalk";
 import dotenv from "dotenv";
 import db from "./db.js"
+import { ObjectId } from "mongodb";
+import joi from "joi";
 
 dotenv.config();
 const app = express();
@@ -13,6 +15,41 @@ app.get("/produtos", async (req, res) => {
     try {
         const produtos = await db.collection("produtos").find({}).toArray();
         res.status(200).send([...produtos]);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+        mongoClient.close();
+    }
+})
+
+app.post("/checkout", async (req, res) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '').trim();
+    if (!token) { return res.sendStatus(401) };
+
+    const dadosBody = joi.object({
+        compras: joi.array().items(joi.object({
+            idProduto: joi.string().required(),
+            quantidade: joi.string().required(),
+            valorUnit: joi.string().required(),
+        })).required(),
+        valorTotal: joi.string().required()
+    })
+    const validacao = dadosBody.validate(req.body, { abortEarly: false });
+
+    if (validacao.error) {
+        console.log(chalk.bold.red("nome não pode ser vazio"), validacao.error.details)
+        res.status(422).send("Use o formato: { name: João}");
+        return;
+    }
+
+    try {
+        const temToken = await db.collection('sessoes').findOne({ token: token });
+        if (!temToken) { res.sendStatus(404); return; }
+        const temUsuario = await db.collection("usuarios").findOne({ _id: ObjectId(temToken.idUsuario) });
+        if (!temUsuario) { res.sendStatus(404); return; }
+        await db.collection("compras").insertOne({ idUsuario: temUsuario._id, ...req.body });
+        res.sendStatus(201);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
