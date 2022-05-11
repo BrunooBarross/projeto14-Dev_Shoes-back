5def7a8cc13398c5bb3d6645098e7efcb40b21f1
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-05-10 22:19:28 -0300

    feat: retirando espaços indesejados do projeto base

--- index.js
+++ index.js
@@ -9,7 +9,5 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-
-
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(chalk.bold.blue(` Servidor rodando na porta ${port}`)));
