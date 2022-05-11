60e6db32359e6305d66b1fb1093b14253770490c
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-05-11 13:05:48 -0300

    feat: adicionando ao projeto a função delete favoritos, recebendo via header o token assim como o id do favorito através do paramêtro

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import chalk from "chalk";
 import dotenv from "dotenv";
 import db from "./db.js"
+import { ObjectId } from "mongodb";
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,24 @@ app.get("/produtos", async (req, res) => {
     try {
         const produtos = await db.collection("produtos").find({}).toArray();
         res.status(200).send([...produtos]);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+        mongoClient.close();
+    }
+})
+
+app.delete("/favoritos/:id", async (req, res) => {
+    const id = req.params.id;
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '').trim();
+    if (!token) { return res.sendStatus(401) };
+
+    try {
+        const temToken = await db.collection('sessoes').findOne({ token: token });
+        if (!temToken) { res.sendStatus(404); return; }
+        await db.collection("favoritos").deleteOne({ _id: ObjectId(id) });
+        res.sendStatus(200);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
