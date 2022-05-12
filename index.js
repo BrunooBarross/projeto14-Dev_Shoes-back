d7523fc60771d6f06a367a6e32e1d3eeead8cb9d
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-05-12 15:49:07 -0300

    feat: Adicionando função post checkout

--- index.js
+++ index.js
@@ -131,5 +131,41 @@ app.post("/favoritos/:id", async (req, res)=>{
     }
 })
 
+app.post("/checkout", async (req, res) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '').trim();
+    if (!token) { return res.sendStatus(401) };
+
+    const dadosBody = joi.object({
+        compras: joi.array().items(joi.object({
+            idProduto: joi.string().required(),
+            quantidade: joi.string().required(),
+            valorUnit: joi.string().required(),
+        })).required(),
+        valorTotal: joi.string().required()
+    })
+    const validacao = dadosBody.validate(req.body, { abortEarly: false });
+
+    if (validacao.error) {
+        console.log(chalk.bold.red("nome não pode ser vazio"), validacao.error.details)
+        res.status(422).send("Use o formato: { name: João}");
+        return;
+    }
+
+    try {
+        const temToken = await db.collection('sessoes').findOne({ token: token });
+        if (!temToken) { res.sendStatus(404); return; }
+        const temUsuario = await db.collection("usuarios").findOne({ _id: ObjectId(temToken.idUsuario) });
+        if (!temUsuario) { res.sendStatus(404); return; }
+        await db.collection("compras").insertOne({ idUsuario: temUsuario._id, ...req.body });
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+        mongoClient.close();
+    }
+})
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(chalk.bold.blue(` Servidor rodando na porta ${port}`)));
