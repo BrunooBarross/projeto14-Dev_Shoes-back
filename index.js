ad7a3c7699db2917270a9441c734591db8da60a0
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-05-14 16:20:44 -0300

    fix: refatorando função perfil para buscar o perfil do usuário com as compras

--- index.js
+++ index.js
@@ -191,30 +191,13 @@ app.get("/perfil", async (req, res) => {
         const temUsuario = await db.collection("usuarios").findOne({ _id: ObjectId(temToken.usuarioID) });
         if (!temUsuario) { res.sendStatus(404); return; }
         const comprasUsuario = await db.collection("compras").find({ idUsuario: temToken.usuarioID}).toArray();
-        res.status(200).send(temUsuario);
+        res.status(200).send([temUsuario, {comprasUsuario: comprasUsuario}]);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
         mongoClient.close();
     }
 })
-app.get("/compras", async (req, res) => {
-    const { authorization } = req.headers;
-    const token = authorization?.replace('Bearer ', '').trim();
-    if (!token) { return res.sendStatus(401) };
 
-    try {
-        const temToken = await db.collection('sessoes').findOne({ token: token });
-        if (!temToken) { res.sendStatus(404); return; }
-        const temUsuario = await db.collection("usuarios").findOne({ _id: ObjectId(temToken.usuarioID) });
-        if (!temUsuario) { res.sendStatus(404); return; }
-        const comprasUsuario = await db.collection("compras").find({ idUsuario: temToken.usuarioID}).toArray();
-        res.status(200).send(comprasUsuario);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-        mongoClient.close();
-    }
-})
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(chalk.bold.blue(` Servidor rodando na porta ${port}`)));
